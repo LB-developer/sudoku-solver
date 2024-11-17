8d8ec7544d50131e8ca7573cdd92a6b424101eeb
lblmdesk <loganbendev@gmail.com>
2024-11-17 13:23:58 +1300

    refactor: simplify solver
    deleted the solver a few days ago and re-wrote the logic to ensure
    understanding
    
    - moved backtracking to a helper function for simplicity

--- solver.go
+++ solver.go
@@ -1,11 +1,13 @@
 package main
 
 func solve(row int, col int, board *[9][9]int, rows *MapOfSets, cols *MapOfSets, sections *MapOfSets) (*[9][9]int, bool) {
+	// incrementation
 	if col == 9 {
 		row++
 		col = 0
 	}
 
+	// base case
 	if row == 9 {
 		if solved := checkBoard(board); solved {
 			return board, true
@@ -14,33 +16,35 @@ func solve(row int, col int, board *[9][9]int, rows *MapOfSets, cols *MapOfSets,
 		}
 	}
 
+	// calculate section
+	section := (row / 3) + 3*(col/3)
+
+	// pre-set board numbers cannot be changed so we skip those
 	if board[row][col] == 0 {
-		section := row/3 + 3*(col/3)
-
 		for n := 1; n <= 9; n++ {
-			_, rFound := (*rows)[row][n]
-			_, cFound := (*cols)[col][n]
-			_, sFound := (*sections)[section][n]
+			// check if the current number is in the current row/col/section
+			_, nInRow := (*rows)[row][n]
+			_, nInCol := (*cols)[col][n]
+			_, nInSection := (*sections)[section][n]
 
-			if rFound || cFound || sFound {
+			if nInRow || nInCol || nInSection {
 				continue
 			}
 
-			board[row][col] = n
-
+			// add the current number to the set
 			(*rows)[row][n] = struct{}{}
 			(*cols)[col][n] = struct{}{}
 			(*sections)[section][n] = struct{}{}
 
-			if _, solved := solve(row, col+1, board, rows, cols, sections); solved {
+			// amend the current board cell with the current number
+			board[row][col] = n
+
+			// try and solve with the current number
+			if _, solved := solve(row, col+1, board, rows, cols, sections); !solved {
+				backtrack(rows, row, cols, col, sections, section, n, board)
+			} else {
 				return board, true
 			}
-			// delete everything from their sets
-			// reset board cell to 0
-			delete((*rows)[row], n)
-			delete((*cols)[col], n)
-			delete((*sections)[section], n)
-			board[row][col] = 0
 
 		}
 	} else {
@@ -49,7 +53,17 @@ func solve(row int, col int, board *[9][9]int, rows *MapOfSets, cols *MapOfSets,
 		}
 	}
 
-	return board, false
+	return nil, false
+}
+
+func backtrack(r *MapOfSets, cr int, c *MapOfSets, cc int, s *MapOfSets, cs int, n int, b *[9][9]int) {
+	// remove current number from the sets
+	delete((*r)[cr], n)
+	delete((*c)[cc], n)
+	delete((*s)[cs], n)
+
+	// reset the current board cell
+	b[cr][cc] = 0
 }
 
 func checkBoard(b *[9][9]int) bool {
